8d947ff6e28904376e6d8a4ab44fe5eab33dc0c6
root <root@ip-172-31-15-203.eu-west-1.compute.internal>
2016-11-10 02:53:22 -0500

    A

--- platform.secrets.sh
+++ platform.secrets.sh
@@ -7,10 +7,10 @@ export INITIAL_ADMIN_USER="juan.smith"
 export INITIAL_ADMIN_PASSWORD_PLAIN="password1234"
 
 # Password for the Jenkins user
-export PASSWORD_JENKINS="a135e6f813e88913"
+export PASSWORD_JENKINS="44d0c025e6f66781"
 
 # Root password for the Sonar and Gerrit MySQL instances
-export PASSWORD_SQL="1dfc353336b21492"
+export PASSWORD_SQL="097466e454bf4250"
 
 # Root password for the LDAP server
-export LDAP_PWD="e3bc02851baa9877" 
+export LDAP_PWD="28b038d081fe2790" 
